29643e77bef1158e91256bcbb7c5bd0cd2289930
Andrew Bellamy <Andrew Bellamy>
2017-07-24 09:35:37 +0930

    initial commit

---
+++ app/src/main/java/com/example/thean/calling/DateSelect.java
@@ -0,0 +1,13 @@
+package com.example.thean.calling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DateSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date_select);
+    }
+}
