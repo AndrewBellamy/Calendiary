4337055e16aae0477ad76b5a5d7521c9a71352a1
AndrewBellamy <andymanbellamy@gmail.com>
2017-07-24 11:21:43 +0930

    adding display note

--- app/src/main/java/com/example/thean/calling/Home.java
+++ app/src/main/java/com/example/thean/calling/Home.java
@@ -2,13 +2,39 @@ package com.example.thean.calling;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 public class Home extends AppCompatActivity {
+
+    private ListView obj;
+    DBHelper mydb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mydb = new DBHelper(this);
+        ArrayList array_list = mydb.getAllNotes();
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, array_list);
+
+        obj = (ListView)findViewById(R.id.noteList);
+        obj.setAdapter(arrayAdapter)
+        obj.setOnItemClickListener(new OnItemClickListener(){
+           public void onItemClick(int arg0) {
+               //method and intent behind selecting a note entry
+               int id_To_open = arg0;
+
+               Bundle dataBundle = new Bundle();
+               dataBundle.putInt("id", id_To_open);
+
+               Intent intent = new Intent(getApplicationContext(), DisplayNote.class);
+               intent.putExtras(dataBundle);
+               startActivity(intent);
+           }
+        });
     }
+
 
 }
